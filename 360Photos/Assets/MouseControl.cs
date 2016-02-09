2d17daebeae6de29d244d418236d5af166f40a14
Matt Mieckowski <matt@lavidavirtual.com>
2016-02-08 19:17:12 -0800

    initial commit

---
+++ 360Photos/Assets/MouseControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseControl : MonoBehaviour
+{
+    float horizontalRotation;
+    float verticalRotation;
+
+    // Use this for initialization
+	void Start () {
+	    print("hello world");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    horizontalRotation += Input.GetAxis("Mouse X");
+	    verticalRotation -= Input.GetAxis("Mouse Y");
+
+	    transform.rotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
+	}
+}
